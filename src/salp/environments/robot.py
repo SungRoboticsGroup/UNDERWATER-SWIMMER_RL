fixed nozzle yaw angle computation

src/salp/environments/robot.py
@@ -63,20 +63,23 @@ class Nozzle:
 
         self.angle2 = np.arccos(2*target_direction[2] - 1)
         if self.angle2 <= -np.pi:
-            self.angle2 += np.pi
+            self.angle2 += 2*np.pi
         elif self.angle2 > np.pi:
-            self.angle2 -= np.pi
+            self.angle2 -= 2*np.pi
 
         if self.angle2 == 0:
             self.angle1 = 0.0
         else:
-            self.angle1 = np.arctan2(np.cos(self.angle2) - 2, np.sqrt(2) * (np.sin(self.angle2)))
+            a = 0.5 * (np.cos(self.angle2) - 1)
+            b = np.sqrt(2) * np.sin(self.angle2) / 2
+            c = target_direction[1]
+            self.angle1 = np.arcsin(c / np.sqrt(a**2 + b**2)) - np.arctan2(b, a)
 
         if self.angle1 <= -np.pi:
-            self.angle1 += np.pi
+            self.angle1 += 2*np.pi
         elif self.angle1 > np.pi:
-            self.angle1 -= np.pi
-
+            self.angle1 -= 2*np.pi
+        
     def get_nozzle_position(self) -> np.ndarray:
         """Calculate the nozzle position in world frame.
         
@@ -782,7 +785,9 @@ if __name__ == "__main__":
     all_drag_torque_data = []
     
     for i in range(n_cycles):
-        robot.set_control(contraction=0.06, coast_time=1, nozzle_angles=np.array([np.pi/2, np.pi]))
+        robot.nozzle.set_yaw_angle(yaw_angle=-np.pi/2)
+        robot.nozzle.solve_angles()
+        robot.set_control(contraction=0.06, coast_time=1, nozzle_angles=np.array([robot.nozzle.angle1, robot.nozzle.angle2]))
         # robot.set_control(contraction=0.06, coast_time=1, nozzle_angles=np.array([0.0, 0.0]))
         robot.step_through_cycle()
     
@@ -853,5 +858,5 @@ if __name__ == "__main__":
     # plot_euler_angles(all_time_data, all_euler_angle_data, all_state_data)
     
     # Plot trajectory in x-y plane with yaw orientation
-    # plot_trajectory_xy(all_position_data, all_state_data, all_euler_angle_data)
+    plot_trajectory_xy(all_position_data, all_state_data, all_euler_angle_data)
     